b8773abf66240f49f9e2061de58294f4122c816c
SoniaBrasa <virtuales@gmx.es>
2023-05-31 01:26:48 +0200

    Cambia a asignación de force a Awake

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,28 +5,23 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     // AddForce magnitude
-    float force;
+    float shotForce;
 
     // Time To Life
     float ttl;
 
 
-    void Start()
+    void Awake()
     {
-        // Para ver despacio
-        force = 100f;
+        shotForce = 20f;
     }
 
-
-    void Update()
-    {
-    }
 
     public void Shot()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
 
         // public void AddForce(Vector3 force, ForceMode mode = ForceMode.Force);
-        rb.AddForce(transform.forward * force, ForceMode.Impulse);
+        rb.AddForce(transform.forward * shotForce, ForceMode.Impulse);
     }
 }
